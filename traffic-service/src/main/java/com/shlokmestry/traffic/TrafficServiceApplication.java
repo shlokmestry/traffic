33a074d1e4d03eacf1843946aa94e8decf049238
shlokmestry <notshlokmestry@gmail.com>
2026-01-29 16:18:33 +0000

    chore: fix application.yaml and set port/redis config

---
+++ traffic-service/src/main/java/com/shlokmestry/traffic/TrafficServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shlokmestry.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficServiceApplication.class, args);
+	}
+
+}
---
+++ traffic-service/src/test/java/com/shlokmestry/traffic/TrafficServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shlokmestry.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
